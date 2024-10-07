a47af110e838c11033ba5dccd102edd46529c274
DziumovS <xbartilaxx@gmail.com>
2024-10-07 16:27:01 +0300

    Added few funcs to game manager to send the same messages (to comply with the DRY principle)

--- src/game/manager.py
+++ src/game/manager.py
@@ -1,6 +1,8 @@
 import random
 from typing import List, Optional, Dict
 
+from fastapi import WebSocket
+
 from src.connection.manager import ConnectionManager
 from src.user.models import Player
 from src.game.models import Game
@@ -36,3 +38,33 @@ class GameManager:
                 if player.user_id == player_id:
                     return game
         return None
+
+    async def send_whose_turn(self, websocket: WebSocket, message: str, user_id: str):
+        await self.connection_manager.send_message(
+            websocket=websocket,
+            message={
+                "type": "whose_turn",
+                "msg": message,
+                "current_player": user_id
+            }
+        )
+
+    async def send_game_data(self, player: Player, current_player: bool, game: Game,
+                             to_dict: bool = True, playable_cards: bool = True):
+        if playable_cards:
+            playable_cards = player.get_playable_cards(current_card=game.current_card, to_dict=to_dict)
+        else:
+            playable_cards = ()
+
+        await self.connection_manager.send_message(
+            websocket=player.websocket,
+            message={
+                "type": "game_data",
+                "hand": player.hand_to_dict(),
+                "playable_cards": playable_cards,
+                "deck_len": len(game.deck),
+                "current_player": current_player,
+                "chosen_suit": game.chosen_suit,
+                "current_card": game.current_card_to_dict(),
+            }
+        )
