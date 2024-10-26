9a98cad247a78c8f8707b6bab81072d640628986
DziumovS <xbartilaxx@gmail.com>
2024-10-26 19:50:03 +0300

    - functionality of preparing playable cards is placed in a separate method; - added data transfer, and already done on the frontend, about the scores rate and the moment when it grows up; - added but not done yet thing about display each player hands; - a lot refactored `disconnect method` but it's not the end..

--- src/game/manager.py
+++ src/game/manager.py
@@ -52,56 +52,63 @@ class GameManager:
     async def send_game_data(self, player: Player, current_player: bool, game: Game,
                              chosen_suit: dict | None = None, playable_cards: bool = True):
 
-        if player.options.must_draw or player.options.must_skip or not playable_cards:
-            playable_cards = ()
-        else:
-            if game.current_card.rank == "J" and game.chosen_suit and game.chosen_suit["chooser_id"] == player.user_id:
-                playable_cards = player.get_playable_cards(current_card=game.current_card, to_dict=True, j=True)
-            else:
-                playable_cards = player.get_playable_cards(
-                    current_card=game.current_card,
-                    chosen_suit=chosen_suit,
-                    to_dict=True
-                )
+        cards = player.prepare_playable_cards(game=game, chosen_suit=chosen_suit, playable_cards=playable_cards)
 
         await self.connection_manager.send_message(
             websocket=player.websocket,
             message={
                 "type": "game_data",
                 "hand": player.hand_to_dict(),
-                "playable_cards": playable_cards,
+                "playable_cards": cards,
                 "deck_len": len(game.deck),
                 "current_player": current_player,
                 "chosen_suit": game.chosen_suit,
                 "current_card": game.current_card_to_dict(),
                 "player_options": player.options_to_dict(),
 
-                "scores_rate": game.deck.scores_rate  # do it!
+                "scores_rate": f"Scores rate: <b>x{game.deck.scores_rate}</b>",
+                "scores_rate_up": True if game.deck.scores_rate > 1 and not game.deck.bounce_deck else False,
+
+                "players_hands": [{"player_id": p.user_id, "hand_len": len(p.hand)} for p in game.players]  # to do
             }
         )
 
+    # start to do
     async def handle_disconnect_game(self, player_id: str, error: bool = False):
         game = self.get_game_by_player_id(player_id=player_id)
         if game:
             left_player = game.get_player_or_none(user_id=player_id)
             game.remove_player(user_id=player_id)
+            message = f"Player {left_player.user_name} has left the game."
 
             while left_player.hand:
                 game.deck.bounce_deck.append(left_player.hand.pop())
 
-            if player_id == game.get_current_player().user_id:
-                game.next_player()
+            try:
+                if player_id == game.get_current_player().user_id:
+                    game.next_player()
+            except IndexError:
+                return
 
             next_player = game.get_current_player()
 
+            await self.connection_manager.broadcast(
+                websockets=game.get_players_websocket(),
+                message={"type": "users_update", "users": game.get_players()}
+            )
+
+            await self.connection_manager.broadcast(
+                websockets=game.get_players_websocket(),
+                message={"type": "show_error", "msg": message}
+            )
+
             for player in game.players:
                 is_current_player = player.user_id == next_player.user_id
-                pre_message = f"Player {left_player.user_name} has left the game. "
                 message = "It's your turn!" if is_current_player else f"It's {next_player.user_name}'s turn!"
 
                 await self.send_whose_turn(
                     websocket=player.websocket,
-                    message=f"{pre_message}{message}",
+                    message=message,
                     user_id=next_player.user_id
                 )
 
@@ -112,23 +119,19 @@ class GameManager:
                     chosen_suit=game.chosen_suit
                 )
 
-                await self.connection_manager.disconnect(websocket=left_player.websocket, error=error)
+            await self.connection_manager.disconnect(websocket=left_player.websocket, error=error)
 
             if len(game.players) <= 1:
                 game.is_active = False
-                message = (
-                    f"With {left_player.user_name} gone, there aren't enough players left in the game to keep it going."
-                )
+                message = (f"{left_player.user_name} has left the game, not enough players to continue the game. "
+                           f"Returning to the home page...")
 
                 await self.connection_manager.broadcast(
                     websockets=game.get_players_websocket(),
-                    message={
-                        "type": "not_enough_players",
-                        "msg": message
-                    }
-                )
+                    message={"type": "not_enough_players", "msg": message})
 
                 for player in game.players:
                     await self.connection_manager.disconnect(websocket=player.websocket)
 
                 self.remove_game(game.game_id)
+    # end to do
