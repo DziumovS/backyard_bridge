d1d64d53ad7e2ac6ce2490f0d377f9cc5f8bd1da
DziumovS <xbartilaxx@gmail.com>
2024-11-10 22:54:49 +0300

    Moved event-handlers into another module; A little refactored code;

--- src/game/router.py
+++ src/game/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
-from src.lobby.router import connection_manager, game_manager
+from src.lobby.router import game_manager
 
 
 router = APIRouter(
@@ -11,15 +11,10 @@ router = APIRouter(
 
 @router.websocket("/ws/game/{game_id}/{user_id}")
 async def websocket_game(websocket: WebSocket, game_id: str, user_id: str):
-    await connection_manager.connect(websocket=websocket)
     game = game_manager.get_game(game_id=game_id)
+    await game_manager.connection_manager.connect(websocket=websocket)
 
-    players = game_manager.get_players(game_id)
-
-    for player in players:
-        if player.user_id == user_id:
-            player.websocket = websocket
-            break
+    game.get_player_or_none(user_id=user_id).websocket = websocket
 
     try:
         while game.is_active:
@@ -28,109 +23,36 @@ async def websocket_game(websocket: WebSocket, game_id: str, user_id: str):
 
             match data["type"]:
                 case "game_started":
-                    player = game.get_player_or_none(user_id=user_id)
-                    current_player = game.get_current_player()
-                    is_current_player = game.is_current_player(player_id=user_id)
-                    message = "It's your turn!" if is_current_player else f"It's {current_player.user_name}'s turn!"
-
-                    await game_manager.send_whose_turn(websocket=player.websocket,
-                                                       message=message,
-                                                       user_id=current_player.user_id)
-
-                    await game_manager.send_game_data(player=player,
-                                                      current_player=is_current_player,
-                                                      game=game,
-                                                      playable_cards=False)
-
-                    if is_current_player and game.first_move:
-                        await connection_manager.send_message(
-                            websocket=current_player.websocket,
-                            message={
-                                "type": "first_turn",
-                                "current_card": game.current_card.card_to_dict()
-                            }
-                        )
+                    await game_manager.event_handler.handle_game_started(player_id=user_id, game=game)
 
                 case "played_card":
-                    current_player = game.get_current_player()
-                    card = current_player.dict_to_card(card=data["card"])
-                    game.current_card = card
-
-                    chosen_suit = data["chosen_suit"]
-                    game.chosen_suit = {
-                        "suit": chosen_suit,
-                        "color": game.deck.color_map[chosen_suit],
-                        "chooser_id": current_player.user_id
-                    } if chosen_suit else None
-
-                    game.handle_special_cards(current_player=current_player, card=card)
-
-                    next_player = game.get_current_player()
-
-                    for player in game.players:
-                        is_current_player = player.user_id == next_player.user_id
-                        message = "It's your turn!" if is_current_player else f"It's {next_player.user_name}'s turn!"
-
-                        await game_manager.send_whose_turn(websocket=player.websocket,
-                                                           message=message,
-                                                           user_id=next_player.user_id)
-
-                        await game_manager.send_game_data(player=player,
-                                                          current_player=is_current_player,
-                                                          game=game,
-                                                          chosen_suit=game.chosen_suit)
-
-                case "drew_card":
-                    current_player = game.get_current_player()
-                    current_player.draw_card(deck=game.deck)
-
-                    playable_cards = current_player.get_playable_cards(
-                        current_card=game.current_card,
-                        chosen_suit=game.chosen_suit
+                    await game_manager.event_handler.handle_played_card(
+                        played_card=data["card"],
+                        chosen_suit=data["chosen_suit"],
+                        game=game
                     )
 
-                    if game.current_card.rank == "6":
-                        game.handle_card_six(current_player=current_player)
-                    else:
-                        if current_player.options.must_draw:
-                            current_player.options.must_draw -= 1
-                        elif current_player.options.must_skip:
-                            current_player.options.must_skip = False
-                        elif current_player.options.can_draw and not playable_cards:
-                            current_player.options.must_skip = True
-                        elif current_player.options.can_draw:
-                            current_player.options.can_draw = False
-                            current_player.options.can_skip = True
-
-                    for player in game.players:
-                        is_current_player = player.user_id == current_player.user_id
-                        await game_manager.send_game_data(player=player, current_player=is_current_player, game=game, chosen_suit=game.chosen_suit)
-
-                    if all((not current_player.options.can_draw, current_player.options.can_skip)):
-                        current_player.set_default_options()
+                case "drew_card":
+                    await game_manager.event_handler.handle_drew_card(game=game)
 
                 case "skip_turn":
-                    current_player = game.get_current_player()
+                    await game_manager.event_handler.handle_skip_turn(game=game)
 
-                    if current_player.options.must_skip:
-                        current_player.options.must_skip = False
+                case 'show_my_move':
+                    await game_manager.event_handler.handle_show_my_move(game=game, data=data)
 
-                    game.next_player()
+                case "game_over":
+                    await game_manager.event_handler.handle_game_over(game=game)
 
-                    next_player = game.get_current_player()
+                case "reset_game":
+                    await game_manager.event_handler.handle_reset_game(game=game)
 
-                    for player in game.players:
-                        is_current_player = player.user_id == next_player.user_id
-                        message = "It's your turn!" if is_current_player else f"It's {next_player.user_name}'s turn!"
-
-                        await game_manager.send_whose_turn(websocket=player.websocket,
-                                                           message=message,
-                                                           user_id=next_player.user_id)
-
-                        await game_manager.send_game_data(player=player, current_player=is_current_player, game=game, chosen_suit=game.chosen_suit)
-
-                case _:
-                    pass
-
-    except WebSocketDisconnect:
-        pass
+    except WebSocketDisconnect as err:
+        match err.code:
+            case 1001 | 1006:
+                await game_manager.event_handler.handle_disconnect_game(player_id=user_id, error=True)
+            case 1012:
+                player = game.get_player_or_none(user_id=user_id)
+                await game_manager.connection_manager.disconnect(websocket=player.websocket, error=True)
+            case _:
+                pass
